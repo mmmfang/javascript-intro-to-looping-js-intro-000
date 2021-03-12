6f67b2cebf235ad08eb2565bfdf4eda03ff1cf78
mmmfang <mae.fang@gmail.com>
2021-03-12 02:34:09 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,33 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+      if (i === 0) {
+          array.push(`I am 0 strange loops.`)
+      } else if (i === 1) {
+          array.push(`I am 1 strange loop.`)
+      } else
+          array.push(`I am ${i} strange loops.`)
+   }return array;
+ }
+
+ function whileLoop(num) {
+   while (num > 0){
+     console.log(num);
+     --num;
+   }
+   return "done";
+  }
+
+
+
+
+  function doWhileLoop(int) {
+    do
+      {
+        function incrementVariable(i) {
+          i = i + 1;
+          return i;
+      }
+      console.log("I run once regardless.");
+    }
+      while (incrementVariable() <= int);
+  }
